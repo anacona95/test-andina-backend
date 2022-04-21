92661c818c7621c8bf20068a9da06d3c91b2a319
anacona95 <71742083+anacona95@users.noreply.github.com>
2022-04-21 02:25:27 -0500

    feat(model): add relationship to the model

--- app/Models/Products_Cities/Products_Cities.php
+++ app/Models/Products_Cities/Products_Cities.php
@@ -2,6 +2,8 @@
 
 namespace App\Models\Products_Cities;
 
+use App\Models\Cities\Cities;
+use App\Models\Products\Products;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +12,12 @@ class Products_Cities extends Model
     use HasFactory;
     protected $table = "products_cities";
     protected $fillable = ['city_id', 'product_id'];
+
+    public function city(){
+        return $this->belongsTo(Cities::class);
+    }
+
+    public function product(){
+        return $this->belongsTo(Products::class);
+    }
 }
