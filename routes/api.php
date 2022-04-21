0c0686f6fee28f723fbe25cb1d7af27c1065e50a
anacona95 <71742083+anacona95@users.noreply.github.com>
2022-04-21 01:01:32 -0500

    refactor(routes): rename api routes
    change productos route nameto products instead

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/productos','App\Http\Controllers\ProductoController@index');//mostrar todos los productos
-Route::post('/productos','App\Http\Controllers\ProductoController@store');//crear un producto
-Route::put('/productos/{id}','App\Http\Controllers\ProductoController@update');//actualizar un producto
-Route::delete('/productos/{id}','App\Http\Controllers\ProductoController@destroy');//eliminar un producto
-Route::get('/ciudades','App\Http\Controllers\CiudadController@index');//mostrar todas las ciudades
+Route::get('/products','App\Http\Controllers\ProductoController@index');//mostrar todos los productos
+Route::post('/products','App\Http\Controllers\ProductoController@store');//crear un producto
+Route::put('/products/{id}','App\Http\Controllers\ProductoController@update');//actualizar un producto
+Route::delete('/products/{id}','App\Http\Controllers\ProductoController@destroy');//eliminar un producto
+Route::get('/cities','App\Http\Controllers\CiudadController@index');//mostrar todas las ciudades
